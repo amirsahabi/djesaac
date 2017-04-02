462673846aa33bcc6bbcd8f3c7f6c128954c0ec5
Amir Sahabi <amirsahabi94@gmail.com>
2017-04-02 01:15:40 -0700

    adding git ignore, also adding more features to app.py, changes to the css table, adding history.html file

--- app/app.py
+++ app/app.py
@@ -8,17 +8,24 @@ app = Flask(__name__)
 # home
 @app.route('/')
 def home():
+    # TODO: Add song listing functionality
     return render_template('home.html')
 
-@app.route('/add', methods=['GET','POST'])
+@app.route('/add/', methods=['GET','POST'])
 def add():
     if(request.method == 'GET'):
         return render_template('add.html')
     else:
         return addSongToQueue(request.form['link'])
 
+@app.route('/history/')
+def history():
+    # TODO: add history functionality
+    return render_template('history.html')
+
+
 def addSongToQueue(songLink):
-    # do some stuff
+    # TODO: add adding song to queue functionality
     return 0
 
 
