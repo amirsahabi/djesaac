4d084c984c000385c4625867dc417b8cf8f84b0b
cobrobrown <cobrobrown@gmail.com>
2017-04-09 01:52:48 -0700

    Issue #13: Thresholded LED values locally in time. Added intensity factor for quiet audible melodies based on logarithmic ratio of the maximum power to the relative local power. Also, preallocated large arrays and eliminated repetitious computation for speed.

--- app/preprocessor.py
+++ app/preprocessor.py
@@ -63,18 +63,7 @@ class SongPreprocessor(threading.Thread):
         counter = 0             # counter of samples
         window = 0.02           # window size = 0.02 seconds
         trigger=0               # used to end a while loop
-        maxpower=0              # modifier for rgb values
         Fs=wave.open(songPath).getframerate()                # default frequency for audio files
-
-
-        #auto determine frequency binning variables
-        lopow=0
-        lofreq=[]
-        mdpow=0
-        mdfreq=[]
-        hipow=0
-        hifreq=[]
-
         winsamples=int(window*Fs)    # number of samples per window
 
         # Import sound file using scipy
@@ -82,109 +71,107 @@ class SongPreprocessor(threading.Thread):
         y=np.array(raw[1])        # convert y to numpy array
         orig=y                    # keep an original copy of y
         songlength = len(y)       # total number of samples in y
+        numsamps=int(songlength/winsamples)
+        #auto determine frequency binning variables
+        lopow=0
+        lofreq=np.zeros(numsamps)
+        mdpow=0
+        mdfreq=np.zeros(numsamps)
+        hipow=0
+        hifreq=np.zeros(numsamps)
+        #relative thresholding variables
+        totalpowerarr=np.zeros(numsamps)
+        powarr=np.zeros((int(winsamples/2)-1,numsamps))
+        maxpower=0
+        index=0
 
-        # Find maxpower of the song
+        # populate arrays for calculation later
         while trigger==0:
             y=orig[counter:counter+winsamples]
             N=len(y)
             if N < winsamples:
                 break
+            #fft and power calculation
             c=np.fft.fft(y)/N
             p=2*(abs(c[1:int(m.floor(N/2))])**2)
             p[:,0]=p[:,0]+p[:,1]
-            f=range(0,int(m.floor(N/2)-1))
-            f=f*(Fs/N)
-            totalpower=np.sum(p[1:len(f),0])
-            # check and update maxpower for this window
+            powarr[:,index]=p[:,0]
+            totalpower=np.sum(p[:,0])
+            totalpowerarr[index]=totalpower
             if totalpower>maxpower:
                 maxpower=totalpower
-                # end totalpower if
-
+            #auto binning: finding frequencies with max powers
             lopowind=np.argmax(p[:15,0])
             lopow+=p[lopowind,0]
-            lofreq.append(lopowind)
+            lofreq[index]=lopowind
             mdpowind=np.argmax(p[10:30,0])
             mdpow+=p[mdpowind,0]
-            mdfreq.append(mdpowind+10)
+            mdfreq[index]=mdpowind+10
             hipowind=np.argmax(p[20:,0])
             hipow+=p[hipowind,0]
-            hifreq.append(hipowind+20)
+            hifreq[index]=hipowind+20
 
-            counter+=winsamples+1
+            counter+=winsamples
             if counter>songlength:
                 trigger=1
                 # end counter if
+            index+=1
             # end trigger while
         trigger=0
         counter=0
+        index=0
 
+        #auto binning frequency calculation
         lomean=np.mean(lofreq)
         mdmean=np.mean(mdfreq)
         himean=np.mean(hifreq)
+        #boundary between lo and mid
         f1=int(m.ceil(lomean+(mdmean-lomean)*mdpow/(mdpow+lopow)))
+        #boundary between mid and hi
         f2=int(m.ceil(mdmean+(himean-mdmean)*hipow/(hipow+mdpow)))
 
-        # set lo, md, and hi value arrays
-        loval=[0]
-        mdval=[0]
-        hival=[0]
+        # preallocate lo, md, and hi value arrays
+        loval=np.zeros(numsamps)
+        mdval=np.zeros(numsamps)
+        hival=np.zeros(numsamps)
+        intfactor=1
+        relthresh=1
+
+
         while trigger==0:
             y=orig[counter:counter+winsamples]
             N=len(y)
-            if N==0:
+            if N<winsamples:
                 break
-            c=np.fft.fft(y)/N
-            p=2*abs(c[1:int(m.floor(N/2))])
-            f=range(0,int(m.floor(N/2)-1))
-            f*=Fs/N
-            totalpower=np.sum(p[1:len(f)])
 
-            lop = np.sum(p[:f1])
-            mdp = np.sum(p[f1:f2])
-            hip = np.sum(p[f2:])
+            lop = np.sum(powarr[:f1,index])
+            mdp = np.sum(powarr[f1:f2,index])
+            hip = np.sum(powarr[f2:,index])
 
-            red = [lop*totalpower/maxpower]
-            grn = [mdp*totalpower/maxpower]
-            blu = [hip*totalpower/maxpower]
+            if index>51 and index<numsamps-51:
+                relthresh=np.sum(totalpowerarr[index-50:index+51])/100.0
+            elif index>51:
+                relthresh=np.sum(totalpowerarr[len(totalpowerarr)-100:])/100.0
+            else:
+                relthresh=np.sum(totalpowerarr[:101])/100.0
 
-            loval = np.concatenate((loval,red),axis=0)
-            mdval = np.concatenate((mdval,grn),axis=0)
-            hival = np.concatenate((hival,blu),axis=0)
+            #intensity factor
+            intfactor=m.log10(maxpower*10.0/relthresh)
+
+            loval[index]=lop/relthresh/intfactor
+            mdval[index]=mdp/relthresh/intfactor
+            hival[index]=hip/relthresh/intfactor
+
+            index+=1
+
             counter+=winsamples+1
             if counter>songlength:
                 trigger = 1
                 # end counter if
             # end trigger while
-        # Adjust light values
-        loval = loval/max(loval)
-        mdval = mdval/max(mdval)
-        hival = hival/max(hival)
 
-        for i in range(1,len(loval)):
-            if loval[i] > .90:
-                hival[i]=0
-                mdval[i]=0
-                loval[i]=1
-                # end loval if
-            elif mdval[i] > .90:
-                loval[i]=0
-                mdval[i]=1
-                hival[i]=0
-                # end mdval if
-            elif hival[i] > .90:
-                loval[i]=0
-                mdval[i]=0
-                hival[i]=1
-                # end hival if
-            # end value for loop
-
-            if loval[i] < 0.04:
-                loval[i] = 0
-            if mdval[i] < 0.04:
-                mdval[i] = 0
-            if hival[i] < 0.04:
-                hival[i] = 0
         self.lovals[songUUID] = loval
         self.mdvals[songUUID] = mdval
         self.hivals[songUUID] = hival
         self.logger.info("Finished preprocessing " + songUUID)
+        #end preprocessSong(self, songPath, songUUID)
