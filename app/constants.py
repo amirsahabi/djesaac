aa69f892be31d61f487ca8b5cad76698cccdb9a0
Amir Sahabi <amirsahabi94@gmail.com>
2017-04-12 08:38:39 -0700

    window_size_sec needs to be an int

--- app/constants.py
+++ app/constants.py
@@ -23,4 +23,4 @@ SUCCESS                     = 'success'
 FAILURE                     = 'failure'
 
 WINDOW_SIZE_MSEC            = 0.02
-WINDOW_SIZE_SEC             = WINDOW_SIZE_MSEC * 1000
+WINDOW_SIZE_SEC             = int(WINDOW_SIZE_MSEC * 1000)
