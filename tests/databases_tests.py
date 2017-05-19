6863749aef512f2a3fabb97179132daaeea99c65
Amir Sahabi <amirsahabi94@gmail.com>
2017-05-18 20:14:56 -0700

    Issue #15: Added more unit tests for databases.py

--- tests/databases_tests.py
+++ tests/databases_tests.py
@@ -2,6 +2,7 @@ from app import databases, constants
 import unittest
 import constants_testing as ct
 import logging
+import uuid
 import os
 
 logging.basicConfig(level=ct.LOG_LEVEL)
@@ -72,6 +73,65 @@ class TestDatabasesMethods(unittest.TestCase):
         assert response_verified.songLink == ct.VALID_DATABASE_STRING
         assert response_verified.songPath == ct.VALID_DATABASE_STRING
         assert response_verified.songTitle == ct.VALID_DATABASE_STRING
+
+    def test_new_bad_preprocess_request(self):
+        response_one = databases.PreprocessRequest.newPreProcessRequest(None, None)
+        response_two = databases.PreprocessRequest.newPreProcessRequest("junk", None)
+
+        assert response_one == constants.FAILED_UUID_STR
+        assert response_two == constants.FAILED_UUID_STR
+
+    def test_new_good_preprocess_request(self):
+        use_uuid = uuid.uuid1()
+        response = databases.PreprocessRequest.newPreProcessRequest(ct.VALID_DATABASE_STRING, use_uuid)
+        assert response != constants.FAILED_UUID_STR
+
+        verify_response = databases.PreprocessRequest.select().where(databases.PreprocessRequest.uuid == response).get()
+        assert verify_response.uuid == response
+        assert verify_response.songPath == ct.VALID_DATABASE_STRING
+        assert verify_response.songUUID == use_uuid
+        assert verify_response.requestType == constants.DB_PREPROC_PROCESS
+
+    def test_new_bad_decomission_request(self):
+
+        response_one = databases.PreprocessRequest.newDecomissionRequest(None)
+        assert response_one == constants.FAILED_UUID_STR
+
+    def test_new_good_decomission_request(self):
+
+        use_uuid = uuid.uuid1()
+        response = databases.PreprocessRequest.newDecomissionRequest(use_uuid)
+        assert response != constants.FAILED_UUID_STR
+
+        verify_response = databases.PreprocessRequest.select().where(databases.PreprocessRequest.uuid == response).get()
+        assert verify_response.uuid == response
+        assert verify_response.songUUID == use_uuid
+        assert verify_response.songPath is None
+        assert verify_response.requestType == constants.DB_PREPROC_DECOMISSION
+
+    def test_check_processing_request(self):
+
+        # test empty
+        use_uuid = uuid.uuid1()
+        response_one = databases.PreprocessRequest.hasntBeenProcessed(None)
+        response_two = databases.PreprocessRequest.hasntBeenProcessed(use_uuid)
+
+        assert response_one is False
+        assert response_two is False
+
+        # insert and check
+        req_id = databases.PreprocessRequest.newPreProcessRequest(ct.VALID_DATABASE_STRING, use_uuid)
+
+        response = databases.PreprocessRequest.hasntBeenProcessed(req_id)
+        assert response is True
+
+        # remove from db
+        databases.PreprocessRequest.delete().where(databases.PreprocessRequest.uuid == req_id).execute()
+
+        # verify has been processed
+        response = databases.PreprocessRequest.hasntBeenProcessed(req_id)
+        assert response is False
+
 
 if __name__ == "__main__":
     unittest.main()
